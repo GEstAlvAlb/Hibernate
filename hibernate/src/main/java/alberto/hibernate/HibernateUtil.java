697e1ca54fee14cc4ee8b85849ec48d73a0b5a1c
alberto <alberto.a.e.g.m1997@hotmail.com>
2019-02-23 18:25:04 +0100

    utiles

--- hibernate/src/main/java/alberto/hibernate/HibernateUtil.java
+++ hibernate/src/main/java/alberto/hibernate/HibernateUtil.java
@@ -1,26 +1,47 @@
 package alberto.hibernate;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-import com.mysql.cj.xdevapi.SessionFactory;
+import alberto.hibernate.Objetos.Autor;
+import alberto.hibernate.Objetos.Juego;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory;
+	private static Session session;
 
 	public static void builSessionFactory() {
-		// TODO Auto-generated method stub
-		
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(Autor.class);
+		configuration.addAnnotatedClass(Juego.class);
+
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
 	}
 
 	public static void closeSessionFactory() {
-		// TODO Auto-generated method stub
-		
+		if (session != null)
+			session.close();
+		if (sessionFactory != null)
+			sessionFactory.close();
 	}
 
 	public static Session getCurrenSession() {
-		// TODO Auto-generated method stub
-		return null;
+
+		if ((session == null) || (!session.isOpen()))
+			openSession();
+
+		return session;
+	}
+
+	public static void openSession() {
+		session = sessionFactory.openSession();
 	}
 
 }
-
